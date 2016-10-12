9d8356042f29c4034fac8bff51b2bbdab8b87500
Ethanjthompson <EthanJ.Thompson@kearsarge.org>
2016-10-12 12:00:55 -0400

    Worked on story

--- story1.py
+++ story1.py
@@ -28,15 +28,24 @@ def story():
     text += "One day he he put a " + noun2 
     text += ". on his " + bodypart1
     text += ". It gave him more self confindence" + Noun3
-    text += ". on his " + bodypart1
-    text += "He went to bed that night feeling " + temperature1
-    text += " with the " +noun2
-    text += ". on his " + bodypart1
-    #text += ". " + 
-    #text += " " + 
-    #text += " " + 
-    #text += " " + 
-    #text += " " + 
+    text += ". He went to bed that night feeling " + feeling1
+    text += "He realized that he wouldent have the " +noun2
+    text += ". for the rest of his life "
+    text += ". The next day " + He/Her/It1
+    text += ". woke up in fright" 
+    text += " A man in a  " + colorshade1
+    text += ". coat was at the foot of his bed"
+    text += "The man told him he needs to come with him." 
+    text += " He told him he needed the " +noun2
+    text += " off his " +bodypart1
+    text += " " +
+    #text += " " +
+    #text += " " +
+    #text += " " +
+    #text += " " +
+    #text += " " +
+    #text += " " +
+      
       
       
     return text
