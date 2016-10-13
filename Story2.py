1a42f62de4a772b0fc98735c4b08cc3bd84edc79
RussellMellott <Russell.mellott@kearsarge.org>
2016-10-13 11:07:14 -0400

    Finished story

--- Story2.py
+++ Story2.py
@@ -1,10 +1,19 @@
 from input import *
 
-#Written by Mr. Spooner
+Written by Russell Mellott
 def story():
     location1 = getWord("Enter a location: ")
-    adjective1 = getWord("Enter a Word: ")
-    name1 = getWord("Enter a Word: ")
+    adjective1 = getWord("Enter a adjective: ")
+    name1 = getWord("Enter a name: ")
+    name2 = getWord("Emter a name: ")
+    adjective2 = getWord("Enter an adjective: ")
+    food1 = getWord ("Enter an food: ")
+    amountoftime1 = getWord ("Enter an amount of time: ") 
+    activeverb1 = getWord ("Enter an active verb: ")
+    verb1 = getWord ("Enter a past tense verb")
+    amountoftime2 = getWord ("Enter an amount of time")
+    sport1 = getWord ("Enter a sport")
+    animal1 = getWord ("Enter an animal")
     
     
     text = ""
@@ -17,16 +26,16 @@ def story():
     text += "and would bring " + food1
     text += "to cookouts every " + amountoftime1 
     text += ". One day however everything went wrong." + name2 
-    text += " decided that he was going to crash the cook out by" + gerundverb1
+    text += " decided that he was going to crash the cook out by" + activeverb1
     text += "everyone there. " + name2
     text += "wanted to do this because Dirty Dan had " + verb1 
     text += "his cat" + amountoftime2 
     text += "ago. As a result of this and" + name2  
-    text += "'s actions, the two of them decided to " + competitiveevent1  
+    text += "'s actions, the two of them decided to play a match of " + sport1  
     text += ". While they did this Dirty Dan was slain in one on one combat against a" + animal1   
     text += "and " + name2 
-    text += "was crowned champion of" + location1 
-    text += ". " 
-   
+    text += "was crowned grand champion of" + location1 
+    text += ". "   
+    
     return text
 
