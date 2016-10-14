47abed816cadf69398d3a624f9a45a4afaa229fe
RussellMellott <Russell.mellott@kearsarge.org>
2016-10-14 11:41:28 -0400

    Finsihed eryting my dud

--- Story2.py
+++ Story2.py
@@ -7,35 +7,35 @@ def story():
     name1 = getWord("Enter a name: ")
     name2 = getWord("Emter a name: ")
     adjective2 = getWord("Enter an adjective: ")
-    food1 = getWord ("Enter an food: ")
+    food1 = getWord ("Enter a food: ")
     amountoftime1 = getWord ("Enter an amount of time: ") 
     activeverb1 = getWord ("Enter an active verb: ")
-    verb1 = getWord ("Enter a past tense verb")
-    amountoftime2 = getWord ("Enter an amount of time")
-    sport1 = getWord ("Enter a sport")
-    animal1 = getWord ("Enter an animal")
+    verb1 = getWord ("Enter a past tense verb: ")
+    amountoftime2 = getWord ("Enter an amount of time: ")
+    sport1 = getWord ("Enter a sport: ")
+    animal1 = getWord ("Enter an animal: ")
     
     
-    text = ""
-    text += "There was once a lad named Dirty Dan, who lived in" + location1
+
+    text = "There was once a lad named Dirty Dan, who lived in " + location1
     text += ". Dans house was " + adjective1
-    text += ". Dirt McDan lived in the house with" + name1 
-    text += " and" + name2 
-    text += ".  " + name1 
-    text += "was really" + adjective2
-    text += "and would bring " + food1
-    text += "to cookouts every " + amountoftime1 
-    text += ". One day however everything went wrong." + name2 
-    text += " decided that he was going to crash the cook out by" + activeverb1
-    text += "everyone there. " + name2
-    text += "wanted to do this because Dirty Dan had " + verb1 
-    text += "his cat" + amountoftime2 
-    text += "ago. As a result of this and" + name2  
+    text += ". Dirt McDan lived in the house with " + name1 
+    text += " and " + name2 
+    text += ". " + name1 
+    text += " was really " + adjective2
+    text += " and would bring " + food1
+    text += " to cookouts every " + amountoftime1 
+    text += ". One day however everything went wrong. " + name2 
+    text += " decided that he was going to crash the cook out by " + activeverb1
+    text += " everyone there. " + name2
+    text += " wanted to do this because Dirty Dan had " + verb1 
+    text += " his cat " + amountoftime2 
+    text += " ago. As a result of this and" + name2  
     text += "'s actions, the two of them decided to play a match of " + sport1  
-    text += ". While they did this Dirty Dan was slain in one on one combat against a" + animal1   
-    text += "and " + name2 
-    text += "was crowned grand champion of" + location1 
-    text += ". "   
+    text += ". While they did this Dirty Dan was slain in one on one combat against a " + animal1   
+    text += " and " + name2 
+    text += " was crowned grand champion of " + location1 
+    text += "."   
     
     return text
 
