91ee56d04517ac66265ca7a6978e0ac9a73c18e2
Ethanjthompson <EthanJ.Thompson@kearsarge.org>
2016-10-17 11:23:14 -0400

    Furnishing's story

--- story1.py
+++ story1.py
@@ -9,8 +9,6 @@ def story():
     noun2 = getWord("Enter a noun: ")
     gender = getWord("Enter another gender: ")
     colorshade1 = getWord("Enter a colorshade: ")
-    noun2 = getWord("Enter a noun: ")
-    bodypart1 = getWord("Enter a body part: ")
     
         #= getWord("Enter a : ")
         #= getWord("Enter a : ")
@@ -22,22 +20,21 @@ def story():
         #= getWord("Enter a : ")
     
     
-    text = "Once apon a time there was a " + gender
+    text = "Once upon a time there was a " + gender
     text += " who wasnt very " + adjective1
-    text += ".One day he he put a " + noun2 
+    text += ". One day, they put a " + noun2 
     text += " on his " + bodypart1
-    text += ". It gave him more self confindence."
+    text += ". It gave them more self confindence."
     text += " He went to bed that night feeling " + feeling1
-    text += ". He realized that he wouldent have the " +noun2
+    text += ". He realized that he wouldnt have the " +noun2
     text += " for the rest of his life."
-    text += " The next day " + gender
-    text += " woke up in fright." 
+    text += " The next day they woke up in fright."
     text += " A man in a " + colorshade1
-    text += " coat was at the foot of their bed."
-    text += "The man told him he needs to come with him." 
+    text += " coat was at the foot of their bed"
+    text += ". The man told him he needs to come with him." 
     text += " He told him he needed the " +noun2
     text += " off his " +bodypart1
-    text += " , its part of a large government conspiracy. "
+    text += " ,its part of a large government conspiracy. "
     #text += " " +
     #text += " " +
     #text += " " +
