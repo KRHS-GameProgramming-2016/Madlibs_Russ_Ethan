649d02580d36d6a4893e4182774b19db81ecf9c1
RussellMellott <Russell.mellott@kearsarge.org>
2016-10-18 11:57:40 -0400

    made dis

--- Story3.py
+++ Story3.py
@@ -2,3 +2,43 @@ from input import *
 
 #Written by Russell Mellott and Ethan J Thompson
 def story():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    text =
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
+    text +=
