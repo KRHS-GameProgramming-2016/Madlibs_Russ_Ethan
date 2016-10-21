310d81c34ea3cd02ab8c8f8a80d02aa4fdbd6051
RussellMellott <Russell.mellott@kearsarge.org>
2016-10-21 11:00:46 -0400

    a

--- Story3.py
+++ Story3.py
@@ -2,43 +2,32 @@ from input import *
 
 #Written by Russell Mellott and Ethan J Thompson
 def story():
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    text =
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
-    text +=
+    name1 = getWord("Enter a name: ")
+    adjective1 = getWord("Enter an adjective: ")
+    MeanName1 = getWord("Enter an insulting name: ")
+    pasttenseverb1= getWord("Enter a past tense verb: ")
+    number1 = getWord("Enter a number: ")
+    pasttenseverb2= getWord("Enter a past tense verb: ")
+    stateOrCountry1 = getWord("Enter a state or country: ")
+    stateOrCountry2 = getWord("Enter another state or country: ")
+   
+    text = "Do you know the muffin man? Well " + name1
+    text += "does. He's a real " + adjective1
+    text += "guy. Not only is he a " + MeanName1
+    text += " but he also " + pasttenseverb1 
+    text += " little kids in the middle of the night. This has gotten him arrested " + number1
+    text += " times. "
+    text += "The Muffin man even" + pasttenseverb2
+    text += " his own family."
+    text += "He's never been outside the borders of" + state/country1
+    text += " because of his probation, and his restraining order against" + name1
+    text += " is expiring next month. " 
+    text += " Now " + name1 
+    text += "is trying to get out of" + state/country1
+    text += " for the fear that the Muffin Man will come after them and " + verb1
+    text += " them again. However because of TRUMPS MAGNUM WALL, immigration out of " + state/country1
+    text += " to anywhere other then " + state/country2 
+    text += " is impossible."
+    text += " RIP " + name1
+    
+    return text
